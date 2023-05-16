3516bc91a7a52013e9cf1be8c3513aaf695fa945
ananichevl <ananichev@phystech.edu>
2023-05-16 14:50:37 +0200

    add qr code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 import './App.css';
 import {Base} from "./component/Base";
 import {Decal} from "./component/Decal";
+import {useState} from "react";
+import {Box, Link, Modal} from "@mui/material";
+import QRCode from "react-qr-code";
 
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 200,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
 function App() {
+    const [isOpen, setIsOpen] = useState(false);
+
+
   return (
       <div style={{height: '480px'}}>
         <h1>Select configuration</h1>
         <Base />
         <Decal />
-        <button style={{marginTop: '20px', width: '60px', height: '60px'}} onClick={() => {
-            window.location.href = 'https://p3d.in/ar/p0da1';
-        }
-        }>AR</button>
+        <button style={{marginTop: '20px', width: '60px', height: '60px'}} onClick={() => setIsOpen(true)}>AR</button>
+          <Modal
+              open={isOpen}
+              onClose={() => setIsOpen(false)}
+              aria-labelledby="parent-modal-title"
+              aria-describedby="parent-modal-description"
+          >
+              <Box sx={style}>
+                  <div>
+                      <QRCode
+                          size={128}
+                          value={'https://p3d.in/ar/p0da1'}
+                      />
+                  </div>
+                  <Link href="https://p3d.in/ar/p0da1">View</Link>
+              </Box>
+          </Modal>
       </div>
   );
 }
